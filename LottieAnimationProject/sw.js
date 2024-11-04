4a61267e5b81396a60552e74209efd42d355e8af
cbeaudry <133229752+cbeaudry@users.noreply.github.com>
2024-11-04 17:05:05 +0100

    Add files via upload

---
+++ LottieAnimationProject/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'lottie-cache-v1';
+const urlsToCache = [
+  '/',                    // Répertoire racine de l’application
+  '/index.html',          // Fichier HTML principal
+  '/manifest.json',       // Manifest de la PWA
+  '/icon-192x192.png',    // Icône
+  '/icon-512x512.png'     // Icône
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
